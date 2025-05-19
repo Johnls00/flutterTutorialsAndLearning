1db4347a1952cf9b1507d06c467f3f25eb655716
John Livingstone <jlivingstone08@qub.ac.uk>
2025-05-19 14:00:26 +0100

    favourites page showing favourited word pairs

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -62,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavouritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -181,6 +181,34 @@ class BigCard extends StatelessWidget {
           semanticsLabel: "{pair.first} {pair.second}",
         ),
       ),
+    );
+  }
+}
+
+class FavouritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if ( appState.favourites.isEmpty) {
+      return Center(
+        child: Text("No favourites yet."),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(padding: const EdgeInsets.all(20),
+        child: Text("You have "
+        "${appState.favourites.length} favourites:"),
+        ),
+        for (var pair in appState.favourites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
+
     );
   }
 }
